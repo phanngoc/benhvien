443088e350378ef1b0a00c8a102caf8d58ccb680
Giang Tran Thi <giangtt@asiantech.vn>
2016-04-28 01:03:43 +0700

    fix section footer

--- resources/views/page-homepage.blade.php
+++ resources/views/page-homepage.blade.php
@@ -18,25 +18,7 @@
 			<div id="wd-wrapper">
 				<header>
 <!-- ============================================= section hompage ============================================ -->
-					<section id="banner" class="each">
-					<!-- ====== menu ===== -->
-						<div class="navbar-fixed">
-							<div id="bg_menu" class="first fadedown">
-								 <nav>
-								    <div class="nav-wrapper">
-								      <a href="#wd-wrapper" class="brand-logo logo">Logo</a>
-								      <ul id="nav-mobile" class="right hide-on-med-and-down">
-								        <li><a href="#sign">ĐĂNG KÍ KHÁM</a></li>
-								        <li><a href="#information">TIN TỨC</a></li>
-								        <li><a href="#news">THÔNG TIN KHOA</a></li>
-								        <li><a href="#find">TRA CỨU</a></li>
-										<li><a href="#response1">Ý KIẾN KHÁCH HÀNG</a></li>
-										<li><a href="#response2">PHẢN HỒI</a></li>
-								      </ul>
-								    </div>
-								  </nav>
-							</div>
-						</div>
+					<section id="banner_page" class="each">
 						<!-- ========== swiper slider ========== -->
 						<div class="swiper-container">
 				        <div class="swiper-wrapper">
@@ -96,29 +78,4 @@
 					</section>
 				</header>
 
-	 <footer class="page-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col l6 s12">
-            <h5 class="white-text">Footer Content</h5>
-            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-          </div>
-          <div class="col l4 offset-l2 s12">
-            <h5 class="white-text">Links</h5>
-            <ul>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="footer-copyright">
-        <div class="container">
-        © 2014 Copyright Text
-        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-        </div>
-      </div>
-    </footer>
 @stop
